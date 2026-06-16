69eeb29ce36deb44794a07404b22003409ea080c
xiaopengchen308-wq <xiaopengchen308@gmail.com>
2026-06-16 16:45:08 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,18 @@
+first_name="xiaopeng"
+last_name="Chen"
+age=17
+used_python_before=False
 
+hobbies= ["video game", "basketball","sleep"]
+favorites= {
+    "movies":"Harry Botter",
+    "food":"Hotpot",
+    "hobby":"Basketball"
+}
+
+print(first_name, last_name, age, used_python_before, hobbies, favorites)
+
+language_spoke = ["English","Mandarin","Canto"]
+skills = ["hard work","team work"]
+
+print(language_spoke, skills)
