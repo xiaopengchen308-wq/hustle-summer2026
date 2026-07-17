6ba382de17d31125e1c60b69d352720349eb0a78
xiaopengchen308-wq <xiaopengchen308@gmail.com>
2026-07-16 21:48:56 -0700

    Create armor.py

---
+++ armor.py
@@ -0,0 +1,10 @@
+#lab 6 | xiaopeng
+import random
+
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        return random.randint(0, self.max_block)
